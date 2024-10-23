7fcd719e25e447c90879277fcb1eee4bfd655449
“Linmaooo234” <zamirina.sofiya@mail.ru>
2024-10-23 17:28:32 +0300

    dfdfdfd

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Slider from "react-slick";
-import s from './slider.module.css';
+import s from './Slider.module.css';
 
 export default function SimpleSlider() {
   const settings = {
